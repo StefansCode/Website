2e5d7f749e7bff1851740c65bee4820b217440ab
StefansCode <schulz.25@gmx.de>
2025-05-28 00:20:21 +0200

    add: upgrade food - Draw expanding circles around the food when throwing it

--- Fishbackground/food.js
+++ Fishbackground/food.js
@@ -3,11 +3,31 @@ class Food{
     constructor(x, y){
         this.x = x;
         this.y = y;
+        this.size = 10;
+        this.radius = 0;
+        this.maxRadius = 200;
     }
 
     draw(ctx){
+        ctx.beginPath();
+
+        // Draw the food
         ctx.fillStyle = 'red';
-        ctx.fillRect(this.x, this.y, 10, 10);
+        ctx.fillRect(this.x - this.size/2, this.y - this.size/2, this.size, this.size);
+
+        // Draw the expanding circle
+        if (this.radius < this.maxRadius){
+            this.radius++;
+            // Calculate opacity based on radius (1 at start, 0 at maxRadius)
+            const opacity = 1 - (this.radius / this.maxRadius);
+            ctx.strokeStyle = `rgba(255, 255, 255, ${opacity})`;
+            ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+            ctx.stroke();
+        }
+
+        ctx.closePath();
     }
-    
+
+
+
 }
